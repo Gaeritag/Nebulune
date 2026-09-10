d9dae2e513287613efc1c790b2741ed1c9351930
Gaeritag <gaeritag@gmail.com>
2026-09-10 22:11:16 +0200

    fix: FishingHelper bobber detection
    * Changed bobber detection to `EntityEvent.Load` and `EntityEvent.Unload` for stability.
    
    NOTE: not fully tested, feel free to dm me if you have any issue with it.

--- src/main/kotlin/foo/starred/nebulune/modules/impl/general/FishingHelper.kt
+++ src/main/kotlin/foo/starred/nebulune/modules/impl/general/FishingHelper.kt
@@ -11,10 +11,11 @@ import foo.starred.athen.modules.Module
 import foo.starred.nebulune.utils.rightClick
 import foo.starred.snowbird.api.client
 import foo.starred.snowbird.api.held
+import foo.starred.snowbird.api.player
 import foo.starred.snowbird.api.scheduling.scheduler.extensions.clientTicks
 import foo.starred.snowbird.api.scheduling.scheduler.extensions.start
 import foo.starred.snowbird.utils.stripped
-import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.projectile.FishingHook
 import net.minecraft.world.item.Items
 
 @Load
@@ -34,34 +35,46 @@ object FishingHelper : Module(
     private val `delay$recast` by config.slider("Recast delay", 1, 0, 10, "ticks")
     private val `variance$recast` by config.slider("Delay variance", 0, 0, 5, "ticks")
 
+    private var bobber: FishingHook? = null
+
     init {
+        on<EntityEvent.Load> {
+            val e = entity as? FishingHook ?: return@on
+            if (e.owner != player) return@on
+            bobber = e
+        }
+
+        on<EntityEvent.Unload> {
+            val e = entity as? FishingHook ?: return@on
+            if (e != bobber) return@on
+            bobber = null
+        }
+
         on<EntityEvent.Update.Named> {
-            val entity = client.player?.fishing as? Entity ?: return@on
+            if (!enabled || !autoPull) return@on
+
+            val hook = bobber ?: return@on
             if (component.stripped() != "!!!") return@on
-            if (entity.distanceTo(entity) > 2f) return@on
+            if (entity.distanceTo(hook) > 2f) return@on
 
-            val a = (`delay$pull` + if (`variance$pull` > 0) (0..`variance$pull`).random() else 0).coerceAtLeast(0)
+            val pullDelay = (`delay$pull` + if (`variance$pull` > 0) (0..`variance$pull`).random() else 0).coerceAtLeast(0)
 
-            Scheduler.schedule(a.clientTicks.start) {
+            Scheduler.schedule(pullDelay.clientTicks.start) {
                 rightClick()
 
                 if (!recast) return@schedule
 
-                val b = 2 + `delay$recast` + if (`variance$recast` > 0) (0..`variance$recast`).random() else 0
-                Scheduler.schedule(b.clientTicks.start) {
+                val recastDelay = 2 + `delay$recast` + if (`variance$recast` > 0) (0..`variance$recast`).random() else 0
+                Scheduler.schedule(recastDelay.clientTicks.start) {
                     rightClick()
                 }
             }
         }
 
         Scheduler.repeat((15 * 20).clientTicks.start) {
-            if (!enabled) return@repeat
-            if (!recast) return@repeat
-            if (!`recast$check`) return@repeat
-            val p = client.player ?: return@repeat
-
-            val b = p.fishing
-            if (b != null && b.isAlive) return@repeat
+            if (!enabled || !recast || !`recast$check`) return@repeat
+            if (client.player == null) return@repeat
+            if (bobber?.isAlive == true) return@repeat
             if (held?.item != Items.FISHING_ROD) return@repeat
 
             rightClick()
